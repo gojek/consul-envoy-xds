87072c653278661c96e3a5f57d5cea80a4656886
Nicholas Whitehead <nwhitehead@squarespace.com>
2018-03-23 07:42:57 -0400

    #6: Added unit test for Pipe based LbEndpoint Address

--- eds/service_host_test.go
+++ eds/service_host_test.go
@@ -13,3 +13,11 @@ func TestShouldSetCPLBAEndpoint(t *testing.T) {
 	ep := sh.LbEndpoint()
 	assert.Equal(t, "socket_address:<address:\"someip\" port_value:9091 > ", ep.Endpoint.Address.String())
 }
+
+func TestShouldSetPipeCPLBAEndpoint(t *testing.T) {
+	sh := eds.NewServiceHost(&api.CatalogService{ServiceAddress: "/tmp/tsd.26333.sock", ServicePort: 0})
+	ep := sh.LbEndpoint()
+	assert.Equal(t, "pipe:<path:\"/tmp/tsd.26333.sock\" > ", ep.Endpoint.Address.String())
+}
+
+
